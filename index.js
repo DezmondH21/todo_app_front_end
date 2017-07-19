e1859c2abe71b4c6d8a58b0e07f0b5de9b28fce3
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-19 17:51:26 +0000

    Vick Picture Corrected

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $ ("#submit").click(function){
+        var todo block=$(this).closet("#new-todo-block")
+            var description= todo block.find(new-todo-description)
+            $("ul").append ("<li>" <input type='checkbox'/>" + description.val() +"<span class='todo'> "+" </span></li>")
+   
+   
+   
+   description.val("")
+   
+            
+    })
+    
+ })
