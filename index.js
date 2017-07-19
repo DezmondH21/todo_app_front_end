389bd8d78c9e5a9b5dc425c5f84c97750c7a16d3
Dezmond Hallman <hallmandezmond@gmail.com>
2017-07-19 20:25:41 +0000

    bodies classed

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $ ("#submit").click(function){
-        var todo block=$(this).closet("#new-todo-block")
-            var description= todo block.find(new-todo-description)
-            $("ul").append ("<li>" <input type='checkbox'/>" + description.val() +"<span class='todo'> "+" </span></li>")
+    /* variables have to be one word and you typed closet instead of closest*/
+    var todo block=$(this).closet("#new-todo-block")
+    /* new-todo-description is missing a hash tag in front*/
+    var description= todo block.find(new-todo-description)
+    $("ul").append ("<li>" <input type='checkbox'/>" + description.val() +"<span class='todo'> "+" </span></li>")
    
    
    
